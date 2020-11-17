3ef3bb98cf7335b783df6ecf01db095ce106d836
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-11-16 20:48:45 -0800

    navbar added and  auth setup

--- my_blogz/settings.py
+++ my_blogz/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'theblog'
+    'theblog',
+    "members",
 
 ]
 
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
