4432ea91fe62fff808dd098a2eca5d70475c1d24
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-11-17 18:29:49 -0800

    psql database setup

---
+++ members/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField
--- my_blogz/urls.py
+++ my_blogz/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theblog.urls')),
     path('members/', include('django.contrib.auth.urls')),
-    path('members/', include('theblog.urls')),
+    path('members/', include('members.urls')),
 ]
