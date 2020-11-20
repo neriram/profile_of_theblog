64c5586de4e993a4e9adaf30f016dd8304600640
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-11-20 09:04:25 -0800

    static file update for deployment

--- my_blogz/settings.py
+++ my_blogz/settings.py
@@ -121,9 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+#Extra places for collectstatic to find static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
